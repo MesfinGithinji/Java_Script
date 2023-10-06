8b929f535cb4abe36d418be7cfb494a8bf13f611
MesfinGithinji <meshgithinji@gmail.com>
2023-10-06 03:05:31 +0300

    Number Datatype and methods

---
+++ nums.js
@@ -0,0 +1,33 @@
+/**
+ * Number represents numeric data type in JS
+ * + operator to add  numbers
+ * - to subtract
+ * * to multiply
+ *  / to divide
+ */
+const a = 10;
+const b = 20;
+const c = a + b;
+console.log(c);
+//output is 30
+
+/**
+ * To increment a number by one in J.S
+ * use the ++ operator
+ * i++ and  i = i+1 are the same
+ */
+
+/**
+ * To decrement a number by one in J.s
+ * use the -- operator
+ * i-- and i=i-1 are the same
+ */
+
+/**
+ * Decimal numbers are also referred to as floating point numbers
+ *
+ *
+ * % modulus operator finds us the remainder of a number
+ */
+
+
