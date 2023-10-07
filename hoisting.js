ed321f083958e65e16114c1489de157747922901
MesfinGithinji <meshgithinji@gmail.com>
2023-10-07 23:43:02 +0300

    J.S refresher

---
+++ hoisting.js
@@ -0,0 +1,30 @@
+//var x = 10;
+//console.log(x);
+
+/**
+ *
+ * 1. structure of webpage - HTML
+   2. Styling of webpage - CSS
+   3. Interactivity / business logic - JS
+*/
+
+/**
+ * Javascript - high-level language , multi-paradigm - OOP , Functional , Imperative and declarative
+ *
+ * J.S original runtime is within a browser
+ * Node.js helps us run js outside the browser
+ *
+ */
+
+/**
+ * variable declaration and initialization
+ *  var old way
+ *  let
+ *  const
+ *
+ */
+
+let x = 10;
+let y = '10';
+
+
