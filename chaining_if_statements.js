db5e70743be984d5f2dcc56bb8e480f5435d7c55
MesfinGithinji <meshgithinji@gmail.com>
2023-10-10 02:06:56 +0300

    If_eles if_

---
+++ chaining_if_statements.js
@@ -0,0 +1,16 @@
+function checkNum(num) {
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "Large";
+    } else if (num >= 20) {
+        return "Huge";
+    } else {
+        return "Not Applicable";
+    }
+}
+console.log(checkNum(30));
